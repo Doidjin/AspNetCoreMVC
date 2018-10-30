7b731a325015e22ba7c35025c919b31e9351f696
Doidjin <LeonardoCabbau@MacBook-Air-de-LEONARDO.local>
2018-10-29 23:33:56 -0300

    Building Walking Skeleton

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
